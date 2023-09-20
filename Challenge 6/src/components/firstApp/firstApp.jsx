f2d4ff16e4f895dc0e05b9b8c01518259e1210b6
Santiago <sanct1318@gmail.com>
2023-09-20 16:38:25 -0500

    Challenge 6

---
+++ Challenge 6/src/components/firstApp/firstApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function FirstApp() {
+    return (
+        <div>
+            <h1>New Title</h1>
+            <span>10</span>
+        </div>
+    )
+}
+
+export default FirstApp
