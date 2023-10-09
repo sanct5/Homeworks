faa79ec852b9b2dd1d09638b6ef7a68b331c9b95
sanct5 <130994767+sanct5@users.noreply.github.com>
2023-10-08 20:13:53 -0500

    Challenge 12
    La API de la presentación ya no funciona profe, no pude testear el useFetch

---
+++ challenge12/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { MultipleCustomHooks } from './components/multipleCustomHooks.jsx/MultipleCustomHooks';
+
+function App() {
+  return (
+    <div className="App">
+      <MultipleCustomHooks />
+    </div>
+  );
+}
+
+export default App;
---
+++ challenge12/src/hooks/useCounter.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export const useCounter = (initialValue) => {
+    const [count, setCount] = useState(initialValue);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrement = () => {
+        setCount(count - 1);
+    };
+
+    const reset = () => {
+        setCount(initialValue);
+    };
+
+    return {
+        count,
+        increment,
+        decrement,
+        reset,
+    };
+}
