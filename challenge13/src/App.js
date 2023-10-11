aca03f2efcbc75a74bda0625f326b7eef58d9194
Santiago <sanct1318@gmail.com>
2023-10-11 15:58:01 -0500

    challenge13

---
+++ challenge13/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Father } from './components/father/Father';
+
+function App() {
+  return (
+    <div className="App">
+      <Father />
+    </div>
+  );
+}
+
+export default App;
